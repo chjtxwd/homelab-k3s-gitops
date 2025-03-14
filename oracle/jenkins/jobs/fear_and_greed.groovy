ece48850ea0dc211b8606cd25762bb87c5891a36
haijin <h.cheng@elsevier.com>
2025-03-14 14:57:34 +0800

    fix: update curl command syntax in deploy step of fear-and-greed job

--- oracle/jenkins/jobs/fear_and_greed.groovy
+++ oracle/jenkins/jobs/fear_and_greed.groovy
@@ -20,24 +20,24 @@ pipelineJob('fear-and-greed') {
                         }
                         stage('Deploy') {
                             steps {
-                                sh '''
-                                    curl 'https://production.dataviz.cnn.io/index/fearandgreed/graphdata' \
-                                      -H 'Accept: */*' \
-                                      -H 'Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7' \
-                                      -H 'Connection: keep-alive' \
-                                      -H 'DNT: 1' \
-                                      -H 'Origin: https://edition.cnn.com' \
-                                      -H 'Referer: https://edition.cnn.com/' \
-                                      -H 'Sec-Fetch-Dest: empty' \
-                                      -H 'Sec-Fetch-Mode: cors' \
-                                      -H 'Sec-Fetch-Site: cross-site' \
-                                      -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36' \
-                                      -H 'sec-ch-ua: "Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"' \
-                                      -H 'sec-ch-ua-mobile: ?0' \
+                                sh \"\"\"
+                                    curl 'https://production.dataviz.cnn.io/index/fearandgreed/graphdata' \\
+                                      -H 'Accept: */*' \\
+                                      -H 'Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7' \\
+                                      -H 'Connection: keep-alive' \\
+                                      -H 'DNT: 1' \\
+                                      -H 'Origin: https://edition.cnn.com' \\
+                                      -H 'Referer: https://edition.cnn.com/' \\
+                                      -H 'Sec-Fetch-Dest: empty' \\
+                                      -H 'Sec-Fetch-Mode: cors' \\
+                                      -H 'Sec-Fetch-Site: cross-site' \\
+                                      -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36' \\
+                                      -H 'sec-ch-ua: "Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"' \\
+                                      -H 'sec-ch-ua-mobile: ?0' \\
                                       -H 'sec-ch-ua-platform: "macOS"' -o result.json
                                     cat result.json | jq .fear_and_greed.score
                                     cat result.json | jq .fear_and_greed.rating
-                                '''
+                                \"\"\"
                             }
                         }
                     }
