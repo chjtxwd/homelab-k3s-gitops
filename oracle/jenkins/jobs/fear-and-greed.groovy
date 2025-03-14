0e0508c32dba42496d24c284db55ccaad690712b
haijin <h.cheng@elsevier.com>
2025-03-14 13:56:19 +0800

    Feat: update fear-and-greed

--- oracle/jenkins/jobs/fear-and-greed.groovy
+++ oracle/jenkins/jobs/fear-and-greed.groovy
@@ -1,11 +1,20 @@
-// Jenkinsfile (Declarative Pipeline)
-pipeline {
-  agent any
-  stages {
-    stage('Stage 1') {
-      steps {
-        echo 'Hello world!'
-      }
+multibranchPipelineJob('fear-and-greed') {
+    displayName('fear-and-greed')
+    description('fear-and-greed')
+    branchSources {
+        git {
+            id('fear-and-greed')
+            remote('https://github.com/chjtxwd/homelab-k3s-gitops')
+        }
     }
-  }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(0)
+        }
+    }
+    configure { node ->
+        node / sources / data / 'jenkins.branch.BranchSource' / source / traits {
+            'jenkins.plugins.git.traits.BranchDiscoveryTrait'()
+        }
+    }
 }
