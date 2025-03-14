0efcb06084ad70011f0a86b81ff4a1f16ae2e793
haijin <h.cheng@elsevier.com>
2025-03-14 15:17:59 +0800

    feat: add jq installation and post fear and greed index score and rating to notification service

--- oracle/jenkins/jobs/fear_and_greed.groovy
+++ oracle/jenkins/jobs/fear_and_greed.groovy
@@ -11,6 +11,8 @@ pipelineJob('fear-and-greed') {
                         stage('Get fear and greed index') {
                             steps {
                                 sh \"\"\"
+                                    apt update
+                                    apt install -y jq
                                     curl 'https://production.dataviz.cnn.io/index/fearandgreed/graphdata' \\
                                       -H 'Accept: */*' \\
                                       -H 'Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7' \\
@@ -25,8 +27,9 @@ pipelineJob('fear-and-greed') {
                                       -H 'sec-ch-ua: "Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"' \\
                                       -H 'sec-ch-ua-mobile: ?0' \\
                                       -H 'sec-ch-ua-platform: "macOS"' -o result.json
-                                    cat result.json | jq .fear_and_greed.score
-                                    cat result.json | jq .fear_and_greed.rating
+                                    score=$(cat result.json | jq .fear_and_greed.score)
+                                    rating=$(cat result.json | jq .fear_and_greed.rating)
+                                    curl -X POST -d "Fear and Greed Index Score: $score, Rating: $rating" https://ntfy.haijin666.top/haijin
                                 \"\"\"
                             }
                         }
