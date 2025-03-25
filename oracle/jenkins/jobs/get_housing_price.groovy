93d62219ede39ae7e04009179002a22c19d6a8ea
chjtxwd <chjtxwd@vip.qq.com>
2025-03-25 22:01:27 +0800

    feat: add Jenkins pipeline job for housing price retrieval with Python script

---
+++ oracle/jenkins/jobs/get_housing_price.groovy
@@ -0,0 +1,29 @@
+pipelineJob('get-housing-price') {
+    displayName('get-housing-price')
+    description('get-housing-price')
+    triggers {
+        cron('0 10,22 * * *')
+    }
+    definition {
+        cps {
+            script(
+                '''
+                pipeline {
+                    agent any
+                    stages {
+                        stage('version') {
+                        steps {
+                            sh 'python3 --version
+                            }
+                            }
+                        }
+                        stage('Get price'){
+                            steps {
+                                sh 'python3 get_housing_price/get_price.py'
+                    }
+                }
+                '''
+            )
+        }
+    }
+}
